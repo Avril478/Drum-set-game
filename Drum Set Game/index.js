ac14574a495f8b4ee61f799c9848ab2e908fbfe9
Avril Wang <83494772+Avril478@users.noreply.github.com>
2024-03-07 16:01:08 +1300

    initial file

---
+++ Drum Set Game/index.js
@@ -0,0 +1,8 @@
+// target.addEventListener("event-type", listener
+// more than one button EventTarget, so document.querySelector("button") will return the first button, not all
+
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert("success");
+    });
+}
